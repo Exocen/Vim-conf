a7e76741ccc992376d82c8e6c99e0753d292210a
Exocen <Exocen@users.noreply.github.com>
2020-09-20 21:30:04 -0400

    added update + fock

---
+++ update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: update.sh
+#
+#         USAGE: ./update.sh
+#
+#   DESCRIPTION:
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (),
+#  ORGANIZATION:
+#       CREATED: 09/20/2020 21:21
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+LOCAL=`dirname "$(readlink -f "$0")"`
+cd "$LOCAL"
+python ~/.vim_runtime/update_plugins.py
+git remote add upstream https://github.com/amix/vimrc.git
+git fetch upstream
+cd -
