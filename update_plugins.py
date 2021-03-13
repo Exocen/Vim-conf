aa05053425c9c0c59cb37d13bd3ad53ad8ef3ca4
Exocen <Exocen@users.noreply.github.com>
2021-03-13 04:55:23 -0500

    removed snipmate

--- update_plugins.py
+++ update_plugins.py
@@ -37,7 +37,6 @@ vim-less https://github.com/groenewege/vim-less
 vim-markdown https://github.com/tpope/vim-markdown
 vim-pyte https://github.com/therubymug/vim-pyte
 vim-python-pep8-indent https://github.com/Vimjas/vim-python-pep8-indent
-vim-snipmate https://github.com/garbas/vim-snipmate
 vim-snippets https://github.com/honza/vim-snippets
 vim-surround https://github.com/tpope/vim-surround
 vim-expand-region https://github.com/terryma/vim-expand-region
